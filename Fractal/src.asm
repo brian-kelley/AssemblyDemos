e998460e9568157868aee9d644d45cc9dc34f697
Brian Kelley <brian.honda11@gmail.com>
2016-09-02 14:23:42 -0500

    Done with Fractal demo

--- Fractal/src.asm
+++ Fractal/src.asm
@@ -13,32 +13,6 @@ section .text
 _iterate:
   push rbp
   mov rbp, rsp
-  jmp .actual
-  ; fp stack has 2: cr, ci
-  ; debugging: multiply cr and ci by 32 and print them
-  ; st0 = cr, st1 = ci
-  ; 2
-  ; push z.imag and z.real (both initially zero)
-  ; testing: cr^2 + ci^2 >= 4
-  fld st0
-  fmul st0
-  fld st2
-  fmul st0
-  faddp st1
-  fistp dword [intTest]
-  mov eax, [intTest]
-  cmp eax, 4
-  jg .asdf1
-  xor rax, rax
-  pop rbp
-  ret
-  .asdf1:
-  xor rax, rax
-  inc rax
-  pop rbp
-  ret
-
-  .actual:
   fldz
   fldz
   ; 4: zr, zi, cr, ci
@@ -107,15 +81,24 @@ _iterate:
 _drawchar:
   push rbp
   mov rbp, rsp
-  test rdi, rdi
-  jnz .inSet
-    mov edi, 0x2A
-    call _putchar
-    jmp .drawDone
-  .inSet:
-    mov edi, 0x2D
-    call _putchar
-  .drawDone:
+  mov rax, [itercap]
+  sub rax, rdi
+  ; if rax == 0: print 0x2A
+  ; else if rax <= 4: print 0x2D
+  ; else: print 0x20
+  cmp eax, 4
+  jg .zone1
+  mov edi, 0x20
+  jmp .print
+  .zone1:
+  cmp eax, 100
+  jg .zone2
+  mov edi, 0x2D
+  jmp .print
+  .zone2:
+  mov edi, 0x23
+  .print:
+  call _putchar
   pop rbp
   ret
 
@@ -159,21 +142,13 @@ _main:
 
 section .rodata
 
-itercap: dq 500
+itercap: dq 500000
 xstart: dd -1.5
-ystart: dd -1.5
-psx: dd 0.0375
-psy: dd 0.107
+ystart: dd -1.2
+psx: dd 0.028
+psy: dd 0.08
 
 section .data
 
 status: dw 0
 intTest: dd 0
-delay: dq 0xFFFFFF
-fmt: db '%i ', 0
-fmt2: db '%x', 0x0A, 0
-fmt3: db '(%3i,%3i) ', 0
-msg1: db 'Starting outer loop', 0
-msg2: db 'Done with iterate()', 0
-debug1: dd 0
-debug2: dd 0
